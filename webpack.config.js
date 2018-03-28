eba6066d055dc854aa9d0812a4ceae09ee46a149
Sylvester Aswin Stanley <sylvester.aswin.stanley@zalando.de>
2018-03-28 10:50:55 +0200

    Updating to react16

--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,57 @@
-import webpack from 'webpack';
-import pkg from './package.json';
-import camelCase from 'camelcase';
-import ExtractTextPlugin from 'extract-text-webpack-plugin';
-
-const capitalizeFirstLetter = string => {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-};
+import webpack from "webpack";
+import pkg from "./package.json";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 const webpackConfig = {
-  output: {
-    filename: `${pkg.name}.js`,
-    library: pkg.moduleName,
-    libraryTarget: 'umd',
-  },
-  externals: {
-    react: {
-      root: 'React',
-      commonjs: 'react',
-      commonjs2: 'react',
-      amd: 'react',
+    output: {
+        filename: `${pkg.name}.js`,
+        library: pkg.moduleName,
+        libraryTarget: "umd"
     },
-    'react-dom': {
-      root: 'ReactDOM',
-      commonjs: 'react-dom',
-      commonjs2: 'react-dom',
-      amd: 'react-dom',
+    mode: "production",
+    externals: {
+        react: {
+            root: "React",
+            commonjs: "react",
+            commonjs2: "react",
+            amd: "react"
+        },
+        "react-dom": {
+            root: "ReactDOM",
+            commonjs: "react-dom",
+            commonjs2: "react-dom",
+            amd: "react-dom"
+        }
     },
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules)/,
-        loader: 'babel-loader',
-      },
-      {
-        test: /\.(scss|css)$/,
-        loader: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
+    module: {
+        rules: [
             {
-              loader: 'css-loader',
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules)/,
+                loader: "babel-loader"
             },
-          ],
+            {
+                test: /\.(scss|css)$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"]
+            }
+        ]
+    },
+    resolve: {
+        modules: ["node_modules", "bower_components"],
+        extensions: [".jsx", ".js"]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            "process.env": {
+                NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+            }
         }),
-      },
-    ],
-  },
-  resolve: {
-    modules: ['node_modules', 'bower_components'],
-    extensions: ['.jsx', '.js'],
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-      },
-    }),
-    new ExtractTextPlugin({
-      filename: `${pkg.name}.css`,
-      allChunks: false,
-    }),
-  ],
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: `${pkg.name}.css`
+        })
+    ]
 };
 
 export default webpackConfig;
